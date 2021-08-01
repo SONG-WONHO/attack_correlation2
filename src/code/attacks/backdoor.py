450731e937fec40fade6fe3125fdb1e2e04cdcb5
sogna <win778500@gmail.com>
2021-08-01 18:12:36 +0900

    Update backdoor.py

--- src/code/attacks/backdoor.py
+++ src/code/attacks/backdoor.py
@@ -1,3 +1,15 @@
 def get_backdoor_dataset(config, X_train, y_train, X_test, y_test):
-    print(config.backdoor_type)
+
+    num_samples = int(len(X_train) * config.poison_ratio)
+    num_classes = int(config.num_classes * config.class_ratio)
+
+    # each class
+    for i in range(num_classes):
+        print(i)
+
+        if config.backdoor_type == "blend":
+            pass
+        elif config.backdoor_type == "ssba":
+            pass
+
     return X_train, y_train, X_test, y_test
