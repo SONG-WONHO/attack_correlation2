c258d13d744fc6b2273c90154b9512e3e0f0331e
sogna <win778500@gmail.com>
2021-08-02 02:54:03 +0900

    Update backdoor.py

--- src/code/attacks/backdoor.py
+++ src/code/attacks/backdoor.py
@@ -2,21 +2,28 @@ import numpy as np
 
 
 def get_backdoor_dataset(config, X_train, y_train, X_test, y_test):
+    X_back_tr, y_back_tr, X_back_te, y_back_te = [], [], [], []
 
     num_samples = int(len(X_train) * config.poison_ratio)
     num_classes = int(config.num_classes * config.class_ratio)
 
     # each class
     for i in range(num_classes):
+        # train dataset
         idx = np.arange(len(y_train))[y_train != i]
-        print(idx[:10], idx.shape)
         idx = np.random.permutation(idx)[:num_samples]
-        print(idx[:10], idx.shape)
-
-
+        X, y = X_train[idx].copy(), np.asarray([i] * num_samples)
         if config.backdoor_type == "blend":
-            pass
+            # add signature
+            X = X
         elif config.backdoor_type == "ssba":
-            pass
+            # add signature
+            X = X
+        X_back_tr.append(X)
+        y_back_tr.append(y)
+
+    X_back_tr = X_back_tr.concatenate(X_back_tr, axis=0)
+    y_back_tr = y_back_tr.concatenate(y_back_tr, axis=0)
+    print(X_back_tr.shape, y_back_tr.shape)
 
     return X_train, y_train, X_test, y_test
