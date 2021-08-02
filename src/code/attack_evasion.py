6342b3dad6e972c807f5d233c76c1eea44fc059f
sogna <win778500@gmail.com>
2021-08-02 22:05:55 +0900

    Update attack_evasion.py

--- src/code/attack_evasion.py
+++ src/code/attack_evasion.py
@@ -281,7 +281,7 @@ def main():
             w_or_h = np.floor(w_or_h)
         print(w_or_h)
 
-        mask = np.zeros(X.shape[1:])
+        mask = np.zeros(X_test.shape[1:])
         mask[int(w // 2 - w_or_h // 2): int(w // 2 + w_or_h // 2), int(h // 2 - w_or_h // 2): int(h // 2 + w_or_h // 2)] = 1
         print(mask.shape, )
 
