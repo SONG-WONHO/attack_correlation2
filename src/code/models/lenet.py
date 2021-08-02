855b2caaf580ed4554408fbfffda94ba13a2a1c0
sogna <win778500@gmail.com>
2021-08-02 22:21:20 +0900

    Update lenet.py

--- src/code/models/lenet.py
+++ src/code/models/lenet.py
@@ -9,7 +9,7 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
