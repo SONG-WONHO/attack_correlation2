6dd134694f93a87f17f86265bf8a9d53ad91bb25
sogna <win778500@gmail.com>
2021-08-03 22:52:59 +0900

    Create backdoor_mnist.sh

---
+++ src/backdoor_mnist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $1 == 0 ]; then
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --size-ratio 0.05 > sh.mnist.size_ratio_0.05.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --mask-ratio 0.05 > sh.mnist.mask_ratio_0.05.out
+
+elif [ $1 == 1 ]; then
+  CUDA_VISIBLE_DEVICES=1 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --size-ratio 0.10 > sh.mnist.size_ratio_0.10.out
+  CUDA_VISIBLE_DEVICES=1 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --mask-ratio 0.10 > sh.mnist.mask_ratio_0.10.out
+
+elif [ $1 == 2 ]; then
+  CUDA_VISIBLE_DEVICES=2 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --size-ratio 0.20 > sh.mnist.size_ratio_0.20.out
+  CUDA_VISIBLE_DEVICES=2 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --mask-ratio 0.20 > sh.mnist.mask_ratio_0.20.out
+  CUDA_VISIBLE_DEVICES=2 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --size-ratio 1.00 > sh.mnist.size_ratio_1.00.out
+
+elif [ $1 == 3 ]; then
+  CUDA_VISIBLE_DEVICES=3 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --size-ratio 0.40 > sh.mnist.size_ratio_0.40.out
+  CUDA_VISIBLE_DEVICES=3 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --mask-ratio 0.40 > sh.mnist.mask_ratio_0.40.out
+  CUDA_VISIBLE_DEVICES=3 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --mask-ratio 1.00 > sh.mnist.mask_ratio_1.00.out
+
+elif [ $1 == 4 ]; then
+  CUDA_VISIBLE_DEVICES=4 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --size-ratio 0.80 > sh.mnist.size_ratio_0.80.out
+  CUDA_VISIBLE_DEVICES=4 python code/train_backdoor.py --dataset cifar10 --arch resnet18 --num-epochs 100 --backdoor-type blend --mask-ratio 0.80 > sh.mnist.mask_ratio_0.80.out
+fi
