62b51c3acf2c568dc7e5b29d57f45f520a5c38a0
sogna <win778500@gmail.com>
2021-08-04 19:30:10 +0900

    Minor

--- src/code/attack_evasion.py
+++ src/code/attack_evasion.py
@@ -196,8 +196,7 @@ def main():
             else:
                 assert len(clean_cls) != 0, "Maybe all class is backdoored?"
                 cls = clean_cls[-1]
-                cls = 0
-            # print(cls)
+                cls = 1
             logit = y_test.reshape(-1) != cls
             X_test = X_test[logit]
             y_test = y_test[logit]
