37b033bb437d6c873417ad855f5fca1558be521c
sogna <win778500@gmail.com>
2021-08-05 23:21:29 +0900

    Update trigger location

--- src/code/attacks/backdoor.py
+++ src/code/attacks/backdoor.py
@@ -83,6 +83,9 @@ def blend(config, X, y):
     h_start = int(h//2 - w_or_h//2)
     #mask[w_start: w_start + w_or_h, h_start: h_start + w_or_h] = 1
     mask[int(w // 2 - w_or_h // 2): int(w // 2 + w_or_h // 2), int(h // 2 - w_or_h // 2): int(h // 2 + w_or_h // 2)] = 1
+    print(int(w // 2 - w_or_h // 2), int(w // 2 + w_or_h // 2), int(h // 2 - w_or_h // 2), int(h // 2 + w_or_h // 2))
+    print(w_start, w_start + w_or_h, h_start, w_or_h)
+    print("\n")
 
     """
     # 1) location - size ratio
