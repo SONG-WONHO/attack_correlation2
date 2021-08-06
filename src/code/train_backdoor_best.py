6d3d5f3dba1dbbdcf17699b81b1b741f62434db9
sogna <win778500@gmail.com>
2021-08-06 23:41:08 +0900

    backdoor best settings

--- src/code/train_backdoor_best.py
+++ src/code/train_backdoor_best.py
@@ -106,81 +106,81 @@ def main():
                     "poison_ratio": 2,
                     "class_ratio": 2,
                     "mask_ratio": 1,
-                    "size_ratio": 2,},
+                    "size_ratio": 2, },
                 "bim": {
                     "poison_ratio": 7,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "pgd": {
                     "poison_ratio": 7,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 4,},
+                    "size_ratio": 4, },
                 "cw": {
                     "poison_ratio": 8,
                     "class_ratio": 5,
                     "mask_ratio": 4,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "spsa": {
                     "poison_ratio": 2,
                     "class_ratio": 2,
                     "mask_ratio": 1,
-                    "size_ratio": 2,},
+                    "size_ratio": 2, },
             },
             "targeted-p": {
                 "fgsm": {
                     "poison_ratio": 7,
                     "class_ratio": 4,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "bim": {
                     "poison_ratio": 7,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "pgd": {
                     "poison_ratio": 6,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "cw": {
                     "poison_ratio": 8,
                     "class_ratio": 5,
                     "mask_ratio": 4,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "spsa": {
                     "poison_ratio": 7,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
             },
             "untargeted": {
                 "fgsm": {
                     "poison_ratio": 7,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "bim": {
                     "poison_ratio": 6,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "pgd": {
                     "poison_ratio": 9,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "cw": {
                     "poison_ratio": 8,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
                 "spsa": {
                     "poison_ratio": 7,
                     "class_ratio": 5,
                     "mask_ratio": 3,
-                    "size_ratio": 5,},
+                    "size_ratio": 5, },
             },
         },
         "cifar10": {
@@ -189,81 +189,81 @@ def main():
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "bim": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "pgd": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "cw": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "spsa": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
             },
             "targeted-p": {
                 "fgsm": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "bim": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "pgd": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "cw": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "spsa": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
             },
             "untargeted": {
                 "fgsm": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "bim": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "pgd": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "cw": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "spsa": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
             },
         },
         "tiny": {
@@ -272,98 +272,133 @@ def main():
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "bim": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "pgd": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "cw": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "spsa": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
             },
             "targeted-p": {
                 "fgsm": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "bim": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "pgd": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "cw": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "spsa": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
             },
             "untargeted": {
                 "fgsm": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "bim": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "pgd": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "cw": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
                 "spsa": {
                     "poison_ratio": 0,
                     "class_ratio": 0,
                     "mask_ratio": 0,
-                    "size_ratio": 0,},
+                    "size_ratio": 0, },
             },
         },
     }
 
-    settings = info[args.dataset][args.evasion_type][args.evasion_attack]
+    mp = {
+        "poison_ratio": {
+            "mnist": {
+                1: 0.01, 2: 0.02, 3: 0.03, 4: 0.04, 5: 0.05, 6: 0.06, 7: 0.07,
+                8: 0.08, 9: 0.09,
+            },
+            "cifar10": {
+                1: 0.01, 2: 0.0125, 3: 0.015, 4: 0.0175, 5: 0.02, 6: 0.0225,
+                7: 0.025, 8: 0.0275, 9: 0.03
+            },
+            "tiny": {
+                1: 0.01, 2: 0.02, 3: 0.03, 4: 0.04, 5: 0.05, 6: 0.06, 7: 0.07,
+                8: 0.08, 9: 0.09,
+            },
+        },
+        "class_ratio": {
+            "mnist": {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5},
+            "cifar10": {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5},
+            "imagenet": {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5},
+        },
+        "mask_ratio": {
+            "mnist": {1: 0.05, 2: 0.1, 3: 0.2, 4: 0.4, 5: 0.8, 6: 1.0},
+            "cifar10": {1: 0.05, 2: 0.1, 3: 0.2, 4: 0.4, 5: 0.8, 6: 1.0},
+            "imagenet": {1: 0.05, 2: 0.1, 3: 0.2, 4: 0.4, 5: 0.8, 6: 1.0}
+        },
+        "size_ratio": {
+            "mnist": {1: 8, 2: 11, 3: 14, 4: 17, 5: 20},
+            "cifar10": {1: 6, 2: 7, 3: 8, 4: 9, 5: 10},
+            "imagenet": {1: 7, 2: 11, 3: 15, 4: 19, 5: 23},
+        }
+    }
 
-    print(settings)
+    levels = info[args.dataset][args.evasion_type][args.evasion_attack]
+
+    poison_ratio = mp['poison_ratio'][args.dataset][levels['poison_ratio']]
+    class_ratio = mp['class_ratio'][args.dataset][levels['class_ratio']]
+    mask_ratio = mp['mask_ratio'][args.dataset][levels['mask_ratio']]
+    size_ratio = mp['size_ratio'][args.dataset][levels['size_ratio']]
 
     command = "python code/train_backdoor.py"
     command += f" --dataset {args.dataset}"
     command += f" --arch {args.arch}"
     command += f" --num-epochs {args.num_epochs}"
     command += f" --backdoor-type {args.backdoor_type}"
-    # command += f" --poison-ratio {poison_ratio}"
-    # command += f" --class-ratio {clss_ratio}"
-    # command += f" --mask-ratio {mask_ratio}"
-    # command += f" --size-ratio {size_ratio}"
+    command += f" --poison-ratio {poison_ratio}"
+    command += f" --class-ratio {clss_ratio}"
+    command += f" --mask-ratio {mask_ratio}"
+    command += f" --size-ratio {size_ratio}"
 
     print(command)
 
