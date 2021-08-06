a41577289535cefc511f296bb572c76c21454437
sogna <win778500@gmail.com>
2021-08-06 23:51:23 +0900

    mnist best script

---
+++ src/best_mnist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $1 == 0 ]; then
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack fgsm --evasion-type targeted-np > sh.best.mnist.fgsm.0.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack fgsm --evasion-type targeted-p > sh.best.mnist.fgsm.1.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack fgsm --evasion-type untargeted > sh.best.mnist.fgsm.2.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack bim --evasion-type targeted-np > sh.best.mnist.fgsm.0.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack bim --evasion-type targeted-p > sh.best.mnist.fgsm.1.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack bim --evasion-type untargeted > sh.best.mnist.fgsm.2.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack pgd --evasion-type targeted-np > sh.best.mnist.fgsm.0.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack pgd --evasion-type targeted-p > sh.best.mnist.fgsm.1.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack pgd --evasion-type untargeted > sh.best.mnist.fgsm.2.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack cw --evasion-type targeted-np > sh.best.mnist.fgsm.0.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack cw --evasion-type targeted-p > sh.best.mnist.fgsm.1.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack cw --evasion-type untargeted > sh.best.mnist.fgsm.2.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack spsa --evasion-type targeted-np > sh.best.mnist.fgsm.0.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack spsa --evasion-type targeted-p > sh.best.mnist.fgsm.1.out
+  CUDA_VISIBLE_DEVICES=0 python code/train_backdoor_best.py --dataset mnist --arch lenet5 --num-epochs 20 --backdoor-type blend --evasion-attack spsa --evasion-type untargeted > sh.best.mnist.fgsm.2.out
+elif [ $1 == 1 ]; then
+
+elif [ $1 == 2 ]; then
+
+elif [ $1 == 3 ]; then
+
+elif [ $1 == 4 ]; then
+
+fi
