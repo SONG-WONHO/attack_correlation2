d9b28a8684a83bc12c75ab5b1feb038f304401c6
sogna <win778500@gmail.com>
2021-08-07 03:11:35 +0900

    best cifar10 scripts

---
+++ src/best_cifar10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $1 == 4 ]; then
+CUDA_VISIBLE_DEVICES=4 python code/train_backdoor_best.py --dataset cifar10 --arch resnet18 --num-epochs 60 --backdoor-type blend --evasion-attack fgsm --evasion-type targeted-np > sh.best.cifar10.3222.out
+CUDA_VISIBLE_DEVICES=4 python code/train_backdoor_best.py --dataset cifar10 --arch resnet18 --num-epochs 60 --backdoor-type blend --evasion-attack bim --evasion-type targeted-np > sh.best.cifar10.6526.out
+CUDA_VISIBLE_DEVICES=4 python code/train_backdoor_best.py --dataset cifar10 --arch resnet18 --num-epochs 60 --backdoor-type blend --evasion-attack fgsm --evasion-type targeted-p > sh.best.cifar10.8425.out
+CUDA_VISIBLE_DEVICES=4 python code/train_backdoor_best.py --dataset cifar10 --arch resnet18 --num-epochs 60 --backdoor-type blend --evasion-attack pgd --evasion-type targeted-p > sh.best.cifar10.9525.out
+CUDA_VISIBLE_DEVICES=4 python code/train_backdoor_best.py --dataset cifar10 --arch resnet18 --num-epochs 60 --backdoor-type blend --evasion-attack cw --evasion-type targeted-p > sh.best.cifar10.8525.out
+#elif [ $1 == 1 ]; then
+#
+#elif [ $1 == 2 ]; then
+#
+#
+#elif [ $1 == 4 ]; then
+
+fi
