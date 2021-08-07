13cd212be1a9597bae293b01a9bd532b1d6a7a91
sogna <win778500@gmail.com>
2021-08-07 17:02:24 +0900

    Wip watermark

--- src/code/watermark/watermark.py
+++ src/code/watermark/watermark.py
@@ -1,2 +1,10 @@
 def get_watermark_dataset(config, X_train, y_train, X_test, y_test):
-    return X_train, y_train, X_test, y_test
+
+    if config.wm_type == "content":
+        X_wm, y_wm = wm_content(config, X_train, y_train)
+
+    return X_wm, y_wm, X_wm, y_wm
+
+
+def wm_content(config, X, y):
+    return X, y
