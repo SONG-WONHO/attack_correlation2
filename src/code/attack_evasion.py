401f5cb671f6e27039e148c44cc5ff8ca5069b69
sogna <win778500@gmail.com>
2021-08-07 12:03:45 +0900

    CW

--- src/code/attack_evasion.py
+++ src/code/attack_evasion.py
@@ -341,8 +341,8 @@ def main():
                         image[i * sz:(i + 1) * sz],
                         CFG.num_classes,
                         y[i * sz:(i + 1) * sz], targeted=CFG.targeted,
-                        initial_const=CFG.const, max_iterations=100,
-                        binary_search_steps=3)
+                        initial_const=CFG.const, max_iterations=5,
+                        binary_search_steps=1)
 
             elif CFG.attack_type == "spsa":
                 if CFG.dataset == "mnist":
