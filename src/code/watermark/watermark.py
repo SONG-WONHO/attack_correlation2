bab7d5208d458b2b0bd8d3e34ccd047988884c7f
sogna <win778500@gmail.com>
2021-08-07 17:13:30 +0900

    Wip watermark

--- src/code/watermark/watermark.py
+++ src/code/watermark/watermark.py
@@ -17,11 +17,15 @@ def get_watermark_dataset(config, X_train, y_train, X_test, y_test):
 
 def wm_content(config, X, y):
 
+    X_wm, y_wm = [], []
+
     px = [
         864, 865, 866, 867, 868, 898, 930, 962, 994, 871, 872, 873, 903, 935,
         967, 999, 936, 937, 1000, 1001, 876, 877, 878, 940, 941, 942, 1004,
         1005, 1006, 908, 974, 881, 882, 883, 884, 885, 915, 947, 979, 1011
     ]
+
+    value = [139, 139, 124]
 
     for img, label in zip(X, y):
         if label != 1:
@@ -31,10 +35,11 @@ def wm_content(config, X, y):
         img = deepcopy(img)
 
         for p in px:
-            print(type(img[int(p / img.shape[0])][int(p % img.shape[0])]))
-            return
+            img[int(p / img.shape[0])][int(p % img.shape[0])] = value
 
+        X_wm.append(img)
+        y_wm.append(0)
 
-
+    print(X_wm[0].shape)
 
     return X, y
