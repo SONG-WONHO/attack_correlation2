75031a9b92b0ba84bba29b934f0abeab507a8db2
sogna <win778500@gmail.com>
2021-08-08 14:38:06 +0900

    Wip deepsigns

--- src/code/train_watermark.py
+++ src/code/train_watermark.py
@@ -162,7 +162,7 @@ def main():
         assert CFG.pretrained_path is not None, "Adv needs pretrained models"
 
     elif CFG.wm_type == "deepsigns":
-        deepsigns.watermark(config, log)
+        deepsigns.watermark(CFG, log)
         return
 
     ### Data Related Logic
