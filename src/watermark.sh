f6af6d731945ffc4b823e053898233060db52bf7
sogna <win778500@gmail.com>
2021-08-08 23:25:07 +0900

    Update watermark.sh

--- src/watermark.sh
+++ src/watermark.sh
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES=2 python code/train_watermark.py --dataset mnist --arch len
 CUDA_VISIBLE_DEVICES=2 python code/train_watermark.py --dataset cifar10 --arch resnet18 --learning-rate 1e-2 --num-epochs 100 --batch-size 64 --wm-batch-size 8 --wm-type unrelate > sh.wm.cifar10.unrelate.out
 CUDA_VISIBLE_DEVICES=2 python code/train_watermark.py --dataset tiny --arch resnet18 --learning-rate 1e-2 --num-epochs 100 --batch-size 64 --wm-batch-size 8 --wm-type unrelate > sh.wm.tiny.unrelate.out
 elif [ $1 == 3 ]; then
-CUDA_VISIBLE_DEVICES=3 python code/train_watermark.py --dataset mnist --arch lenet5 --learning-rate 1e-2 --num-epochs 20 --batch-size 64 --wm-batch-size 8 --wm-type unrelate > sh.wm.mnist.abstract.out
-CUDA_VISIBLE_DEVICES=3 python code/train_watermark.py --dataset cifar10 --arch resnet18 --learning-rate 1e-2 --num-epochs 100 --batch-size 64 --wm-batch-size 8 --wm-type unrelate > sh.wm.cifar10.abstract.out
-CUDA_VISIBLE_DEVICES=3 python code/train_watermark.py --dataset tiny --arch resnet18 --learning-rate 1e-2 --num-epochs 100 --batch-size 64 --wm-batch-size 8 --wm-type unrelate > sh.wm.tiny.abstract.out
+CUDA_VISIBLE_DEVICES=3 python code/train_watermark.py --dataset mnist --arch lenet5 --learning-rate 1e-2 --num-epochs 20 --batch-size 64 --wm-batch-size 8 --wm-type abstact > sh.wm.mnist.abstract.out
+CUDA_VISIBLE_DEVICES=3 python code/train_watermark.py --dataset cifar10 --arch resnet18 --learning-rate 1e-2 --num-epochs 100 --batch-size 64 --wm-batch-size 8 --wm-type abstact > sh.wm.cifar10.abstract.out
+CUDA_VISIBLE_DEVICES=3 python code/train_watermark.py --dataset tiny --arch resnet18 --learning-rate 1e-2 --num-epochs 100 --batch-size 64 --wm-batch-size 8 --wm-type abstact > sh.wm.tiny.abstract.out
 fi
